ded659252455945348f7b3d288d2b2c7ed1e5e3e
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-03-13 23:19:30 +0300

    git init

---
+++ src/main/java/com/example/iPhoto/IPhotoApplication.java
@@ -0,0 +1,13 @@
+package com.example.iPhoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IPhotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iPhoto/IPhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iPhoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IPhotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
